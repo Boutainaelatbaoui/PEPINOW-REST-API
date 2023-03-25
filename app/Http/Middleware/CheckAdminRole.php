4ae1bd9b77327095a63447e92f9467717d6f16a4
Boutainaelatbaoui <boutaina.elatbaoui1998@gmail.com>
2023-03-25 14:50:14 +0000

    Added CheckSellerRole and CheckAdminRole middleware

--- app/Http/Middleware/CheckAdminRole.php
+++ app/Http/Middleware/CheckAdminRole.php
@@ -5,15 +5,17 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class CheckAdminRole
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->user()->hasRole('admin')) {
-            return response()->json(['message' => 'Only admins have this role'], 401);
+        if (Auth::user() && Auth::user()->role === 1) {
+            return $next($request);
         }
 
-        return $next($request);
+        return response()->json(['message' => 'Only admins have this role'], 401);
+        
     }
 }
